2d68cc1e6c6a0ea47ae6c1328e2391994a385071
Daniel Ridley <ridleydan31@gmail.com>
2018-09-13 11:37:14 -0500

    brought response array from server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,9 +2,23 @@ console.log('js');
 
 const shellApp = angular.module('ShellApp',[]);
 
-shellApp.controller('ShellController', [function(){
+shellApp.controller('ShellController', ['$http', function($http){
     let self = this;
     self.number = 7;
     self.message = 'Hi-diddly-hey'
+
+    $http({
+        method: 'GET',
+        url: '/shells'
+    }).then(function(response){
+        console.log(response)
+        self.shells= response.data;        
+    }).catch(function(error){
+        alert()
+        console.log('error: ', error);
+        
+    })
+
+
 
 }]);
--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ app = express();
 const bodyParser = require('body-parser');
 //globals
 const PORT = 5000;
+let shells = [{ type: 'Conch', color: 'pink' },
+{ type: 'Blue', color: 'blue' }]
 //uses
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //spins
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server is up and running on ${PORT}`);
-    
+
+})
+
+app.get('/shells', (req, res) => {
+    console.log('in / GET');
+    res.send(shells);
 })
